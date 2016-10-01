d25eb2ddc96038e1b6ee0c7e2569fbfa05eb09ab
Ashley Williams <ashley666ashley@gmail.com>
2016-10-01 17:52:05 -0400

    add random crab photo backgrounds

--- src/templates.rs
+++ src/templates.rs
@@ -11,6 +11,8 @@ use pencil::{Request, PencilResult};
 pub fn motivation(request: &mut Request) -> PencilResult {
     let mut context = BTreeMap::new();
     let motivation = pick_one::pick_one_str(&MOTIVATIONS).to_string();
+    let crab = pick_one::pick_one_str(&["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"]).to_string();
     context.insert("motivation".to_string(), motivation);
+    context.insert("image".to_string(), crab);
     return request.app.render_template("motivation.html", &context);
 }
