da468ecf7a13f900dd10ce6b29b7eb65751fff96
Ashley Williams <ashley666ashley@gmail.com>
2016-10-01 16:05:44 -0400

    make heroku work

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ extern crate pencil;
 extern crate motivations;
 extern crate pick_one;
 
+use std::env;
+use std::str::FromStr;
+
 use motivations::MOTIVATIONS;
 
 use pencil::{Pencil, Request, Response, PencilResult};
@@ -10,13 +13,20 @@ fn hello(_: &mut Request) -> PencilResult {
     Ok(Response::from(pick_one::pick_one_str(&MOTIVATIONS)))
 }
 
+/// Look up our server port number in PORT, for compatibility with Heroku.
+fn get_server_port() -> u16 {
+    let port_str = env::var("PORT").unwrap_or(String::new());
+    FromStr::from_str(&port_str).unwrap_or(7878)
+}
+
 fn main() {
     let mut app = Pencil::new("/web/hello");
     app.get("/", "hello", hello);
     
-    let host = "127.0.0.1";
-    let port = "7878";
+    let host = "0.0.0.0";
+    let port = get_server_port();
+    let address = format!("{}:{}", host, port);
 
-    println!("* Running on http://{}:{}/", host, port);
-    app.run("127.0.0.1:7878");
+    println!("* Running on http://{}", address);
+    app.run(&address[..]);
 }
